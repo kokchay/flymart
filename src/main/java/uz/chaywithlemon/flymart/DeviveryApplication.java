eee4f061d90d3aebca533cf741a1513311bc1d22
uteniyazovabubakir <uteniyazovabubakir@gmail.com>
2024-05-28 16:20:25 +0500

    init

---
+++ src/main/java/uz/chaywithlemon/flymart/DeviveryApplication.java
@@ -0,0 +1,13 @@
+package uz.chaywithlemon.flymart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/chaywithlemon/flymart/DeviveryApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.chaywithlemon.flymart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
